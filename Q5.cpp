9e740bdcac74a7464c1148595919735e8a250a1d
Debarshi1729 <134997858+Debarshi1729@users.noreply.github.com>
2023-07-03 20:20:14 +0530

    Update Q5.cpp

--- Q5.cpp
+++ Q5.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 int main()
 {
-   cout<<"The sizeof(char):"<<sizeof(char)<<endl;
-   cout<<"The sizeof(short):"<<sizeof(short)<<endl;
-   cout<<"The sizeof(int):"<<sizeof(int)<<endl;
-   cout<<"The sizeof(long):"<<sizeof(long)<<endl;
-   cout<<"The sizeof(long long):"<<sizeof(long long)<<endl;
-   cout<<"The sizeof(float):"<<sizeof(float)<<endl;
-   cout<<"The sizeof(double):"<<sizeof(double)<<endl;
-   cout<<"The sizeof(long double):"<<sizeof(long double)<<endl;
-   cout<<"The sizeof(bool):"<<sizeof(bool)<<endl;
+   cout<<"The sizeof(char):"<<sizeof(char)<<"bytes"<<endl;
+   cout<<"The sizeof(short):"<<sizeof(short)<<"bytes"<<endl;
+   cout<<"The sizeof(int):"<<sizeof(int)<<"bytes"<<endl;
+   cout<<"The sizeof(long):"<<sizeof(long)<<"bytes"<<endl;
+   cout<<"The sizeof(long long):"<<sizeof(long long)<<"bytes<<endl;
+   cout<<"The sizeof(float):"<<sizeof(float)<<"bytes"<<endl;
+   cout<<"The sizeof(double):"<<sizeof(double)<<"bytes"<<endl;
+   cout<<"The sizeof(long double):"<<sizeof(long double)<<"bytes"<<endl;
+   cout<<"The sizeof(bool):"<<sizeof(bool)<<"bytes"<<endl;
    return 0;
 }
